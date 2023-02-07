ccaf27b87a6c1cb9cff31881f2785add248ecbb5
Ahmad Rulim <personal@rulim34.dev>
2023-02-07 10:23:15 +0700

    project: Initial commit
    Signed-off-by: Ahmad Rulim <personal@rulim34.dev>

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ['id-ID'],
+    defaultLocale: 'id-ID',
+  },
+  experimental: {
+    appDir: true,
+  },
+}
