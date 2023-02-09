af82c2cb40c0e51830d86b57977c76f9184083b6
Ahmad Rulim <personal@rulim34.dev>
2023-02-09 09:05:14 +0700

    feat(style): Setup Tailwind font and global styles
    Signed-off-by: Ahmad Rulim <personal@rulim34.dev>

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,23 @@
+import { Poppins } from '@next/font/google';
+import { ReactNode } from 'react';
+import '@/presentation/styles/globals.css';
+
+type Props = {
+  children: ReactNode;
+};
+
+const poppins = Poppins({
+  variable: '--font-poppins',
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export default function FrontLayout({ children }: Props) {
+  return (
+    <html lang="id-ID" className={poppins.variable}>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-poppins)', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
